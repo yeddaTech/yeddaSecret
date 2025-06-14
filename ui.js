c0d01fbee216a9c7bd58c2355a536e64a4ecbbbb
Younesse Eddassouli <esyoun70@gmail.com>
2025-06-14 17:42:59 +0200

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,12 @@
+export function setupUI() {
+    document.getElementById('resetBtn').addEventListener('click', resetCamera);
+    document.getElementById('wireframeBtn').addEventListener('click', toggleWireframe);
+}
+
+function resetCamera() {
+    // Logica per reset camera...
+}
+
+function toggleWireframe() {
+    // Logica per wireframe...
+}
